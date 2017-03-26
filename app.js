9e0b0313b240f944befe49e23f5cbb2cbbefd039
顾尧 <gamesgao@163.com>
2017-03-26 21:46:50 +0800

    添加cookie后可以正常访问页面了

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 function getTrueRoomID() {
     request.get("http://www.pixiv.net/member.php?id=4364687")
         .timeout(3000)
+        // .set("Cookie", "")
         .end(function(err, res) {
             if (err) {
                 console.log("Error!");
