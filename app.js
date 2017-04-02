990539b2a8780d42525ceb96589cc12c72da31ee
顾尧 <gamesgao@163.com>
2017-04-02 14:06:34 +0800

    分离cookie，添加队列模式

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var request = require('superagent');
 var cheerio = require('cheerio');
 var fs = require('fs');
+var db = require("mongoDB");
 var $;
 
-function getTrueRoomID() {
-    request.get("http://www.pixiv.net/member.php?id=4364687")
+function getmember(userID) {
+    // 个人简介
+    var cookie = fs.readFileSync("Cookie.txt", "UTF8");
+
+    request.get("http://www.pixiv.net/member.php?id=" + userID)
         .timeout(3000)
-        .set("Cookie", "p_ab_id=4; p_ab_id_2=5; _ga=GA1.2.245833915.1490266995; device_token=617ae967b7e0942b131ad4ebd4cf544e; login_ever=yes; PHPSESSID=9105635_977291571094684f50a5bd992815c404; a_type=0; module_orders_mypage=%5B%7B%22name%22%3A%22recommended_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22fanbox%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; is_sensei_service_user=1; __utmt=1; __utma=235335808.245833915.1490266995.1490535063.1490955179.6; __utmb=235335808.2.10.1490955179; __utmc=235335808; __utmz=235335808.1490513215.3.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=9105635=1^9=p_ab_id=4=1^10=p_ab_id_2=5=1^12=fanbox_subscribe_button=orange=1^13=fanbox_fixed_otodoke_naiyou=yes=1^14=hide_upload_form=yes=1^15=machine_translate_test=no=1")
+        .set("Cookie", cookie)
         .end(function(err, res) {
             if (err) {
                 console.log("Error!");
@@ -23,14 +27,48 @@ function getTrueRoomID() {
             });
 
             $ = cheerio.load(res.text);
-            var temp = $("div.works-illust").children("ul");
-            console.log(temp);
-            // temp.each(function(index, element) {
-            //     console.log(element);
-            // }, this);
+            // 读出前几个发表的作品
+            var temp = $("div.works-illust").children("ul").children("li");
+
+            temp.each(function(index, element) {
+                // console.log(element);
+                console.log(element.children[1].children[0].attribs["title"]);
+            }, this);
 
             // var TrueRoomID = res.text.match(/var ROOMID = (\d*?);/)[1];
         })
 }
 
-getTrueRoomID();
+function getBookmark(userID, index) {
+    // 关注
+    var cookie = fs.readFileSync("Cookie.txt", "UTF8");
+
+    request.get("http://www.pixiv.net/bookmark.php?type=user&id=" + userID + "&rest=show&p=" + index)
+        .timeout(3000)
+        .set("Cookie", cookie)
+        .end(function(err, res) {
+            if (err) {
+                console.log("Error!");
+            }
+
+            fs.writeFile('./bookmark.html', res.text, function(err) {
+                if (err) {
+                    console.log("Error!");
+                }
+                console.log('Saved.');
+            });
+
+            $ = cheerio.load(res.text);
+
+            var temp = $("div.members").children("ul").children("li");
+            temp.each(function(index, element) {
+                // console.log(element);
+                console.log(element.children[0].children[0].attribs["data-user_id"]);
+            });
+
+
+
+        })
+}
+
+getBookmark(30959, 10);
